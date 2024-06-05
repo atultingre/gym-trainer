a0f94a5ccec30948ee609729c96765136d6b8374
Atul Tingre <atultingre.work@gmail.com>
2024-06-05 13:46:12 +0530

    initial setup

---
+++ app/components/Comments/Comments.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Comments = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-evenly p-18" id="comments">
+      Comments
+    </main>
+  );
+};
+
+export default Comments;
---
+++ app/components/results/results.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Results = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-evenly p-18">
+      Results
+    </main>
+  );
+};
+
+export default Results;
